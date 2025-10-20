de685d5c801734731e9d621e1cd3c4c5c9e2b819
Marcel Planas <marcel.artemis@gmail.com>
2025-10-20 19:21:25 +0200

    fix: fixed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
+    fontFamily: {
+      sans: ["ABeeZee", "sans-serif"],
+    },
     container: { center: true, padding: { DEFAULT: "16px", lg: "24px" } },
     screens: {
       xs: "360px",
@@ -10,9 +13,6 @@ export default {
       lg: "1024px",
       xl: "1280px",
       "2xl": "1440px",
-      fontFamily: {
-        sans: ['"ABeeZee"', "ui-sans-serif", "system-ui", "sans-serif"],
-      },
     },
   },
 };
